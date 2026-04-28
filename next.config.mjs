3261c3dc1483be150e53b0c8954750e4a92ee448
Emogyreunion <mv7786986@gmail.com>
2026-04-28 14:01:44 +0300

    add: add logic to reverse proxy frontend requests to the backend:

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,35 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      // Development (local Flask)
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '5000',
+        pathname: '/api/send_image/**',
+      },
+
+      // Production (Flask behind Nginx)
+      {
+        protocol: 'https',
+        hostname: '#',
+        pathname: '/api/send_image/**',
+      },
+    ],
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: `${process.env.BACKEND_URL}/api/:path*`,
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
