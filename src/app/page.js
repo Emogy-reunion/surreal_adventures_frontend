9407961785d574ae79605634874e9e1fedb44a93
Emogyreunion <mv7786986@gmail.com>
2026-04-29 11:31:20 +0300

    add: add the why choose use section to the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Hero from "@/components/landing/Hero";
+import WhyUs from "@/components/landing/WhyUs";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
+      <WhyUs />
     </main>
   );
 };
