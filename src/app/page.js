b36c547d8755ecc8d1de41fb6c258a6332539dc2
Emogyreunion <mv7786986@gmail.com>
2026-04-29 12:12:57 +0300

    add: create the how it works components, add css to style it, add the component to the page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/landing/Hero";
 import WhyUs from "@/components/landing/WhyUs";
+import HowItWorks from "@/components/landing/HowItWorks";
 
 const HomePage = () => {
   return (
     <main>
       <Hero />
       <WhyUs />
+      <HowItWorks />
     </main>
   );
 };
