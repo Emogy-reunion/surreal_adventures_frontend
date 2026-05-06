a9ccbd6bd886b505b389feddc3eac0f19b26794a
Emogyreunion <mv7786986@gmail.com>
2026-05-07 02:43:41 +0300

    add: add the navigation bar and the footer for the home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,17 +2,20 @@ import Hero from "@/components/landing/Hero";
 import WhyUs from "@/components/landing/WhyUs";
 import HowItWorks from "@/components/landing/HowItWorks";
 import CTA from "@/components/landing/CTA";
-
+import NavBar from "@/components/navbar/NavBar";
+import Footer from "@/components/footer/Footer";
 
 const HomePage = () => {
-  return (
-    <main>
-      <Hero />
-      <WhyUs />
-      <HowItWorks />
-      <CTA />
-    </main>
-  );
+	return (
+		<main>
+		  	<NavBar />
+      			<Hero />
+ 	     		<WhyUs />
+      			<HowItWorks />
+	      		<CTA />
+      			<Footer />
+    		</main>
+	);
 };
 
 export default HomePage;
